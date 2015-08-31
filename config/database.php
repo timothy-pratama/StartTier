5515796dc2f3489d802b60d59d4160fbf5e62425
Timothy Pratama <timothy.pratama@gmail.com>
2015-08-31 18:08:05 +0700

    repush .env

--- config/database.php
+++ config/database.php
@@ -64,12 +64,25 @@ return [
             'strict'    => false,
         ],
 
+        /* localhost */
+//        'pgsql' => [
+//            'driver'   => 'pgsql',
+//            'host'     => env('DB_HOST', 'localhost'),
+//            'database' => env('DB_DATABASE', 'forge'),
+//            'username' => env('DB_USERNAME', 'forge'),
+//            'password' => env('DB_PASSWORD', ''),
+//            'charset'  => 'utf8',
+//            'prefix'   => '',
+//            'schema'   => 'public',
+//        ],
+
+        /* production */
         'pgsql' => [
             'driver'   => 'pgsql',
-            'host'     => env('DB_HOST', 'localhost'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host'     => parse_url(getenv("DATABASE_URL"))["host"],
+            'database' => substr(parse_url(getenv("DATABASE_URL"))["path"], 1),
+            'username' => parse_url(getenv("DATABASE_URL"))["user"],
+            'password' => parse_url(getenv("DATABASE_URL"))["pass"],
             'charset'  => 'utf8',
             'prefix'   => '',
             'schema'   => 'public',
